4c33222fb2d93f62ff8510e683d8326eade6ff11
Alin Posorovaschi <alinposho@yahoo.com>
2015-09-25 22:37:55 +0300

    Solution that passes all the tests

--- src/numberprettifier/core.clj
+++ src/numberprettifier/core.clj
@@ -1,18 +1,24 @@
 (ns numberprettifier.core
   (:gen-class))
 
-(def ^:private trillion 1000000000000N)
-(def ^:private billion 1000000000N)
-(def ^:private million 1000000)
+(def trillion 1000000000000N)
+(def billion  1000000000N)
+(def million  1000000)
+
+(defn prettify [number qt]
+  "Returns a prettified version of the "
+  (if (zero? (mod number qt))
+    (str(quot number qt))
+    (format "%.1f"(/ (double number) qt))))
 
 
 (defn number-prettifier
   "Returns a prettified version of the number, i.e. 1000000 will become 1M and so on."
   [number]
   (cond
-    (<= 1 (quot number trillion)) (str (quot number trillion) "T")
-    (<= 1 (quot number billion)) (str (quot number billion) "B")
-    (<= 1 (quot number million)) (str (quot number million) "M")
+    (<= 1 (quot number trillion)) (str (prettify number trillion) "T")
+    (<= 1 (quot number billion)) (str (prettify number billion) "B")
+    (<= 1 (quot number million)) (str (prettify number million) "M")
     :else (str number)))
 
 
--- test/numberprettifier/core_test.clj
+++ test/numberprettifier/core_test.clj
@@ -8,9 +8,23 @@
     (is (= "2.5M" (number-prettifier 2500000.35)))
     (is (= "532" (number-prettifier 532)))
     (is (= "1.1B" (number-prettifier 1123456789)))
-    (is (= "2.9T" (number-prettifier 2923456789000000N)))
+    (is (= "2.9T" (number-prettifier 2923456789000N)))
+    (is (= "2923.5T" (number-prettifier 2923456789000000N)))
     (is (= "0" (number-prettifier 0)))
     (is (= "-1" (number-prettifier -1)))
     (is (= "-1000000" (number-prettifier -1000000)) "Negative numbers are not prettified")
+    ))
+
+(deftest divide-test
+  (testing "The number-prettifier function"
+    (is (= "1" (prettify 1000000 million)))
+    (is (= "2.5" (prettify 2500000.35 million)))
+    (is (= "0.0" (prettify 532 million)))
+    (is (= "1.1" (prettify 1123456789 billion)))
+    (is (= "2923.5" (prettify 2923456789000000N trillion)))
+    (is (= "2.9" (prettify 2923456789000N trillion)))
+    (is (= "0" (prettify 0 million)))
+    (is (= "-0.0" (prettify -1 million)))
+    (is (= "-1" (prettify -1000000 million)))
     ))
 
